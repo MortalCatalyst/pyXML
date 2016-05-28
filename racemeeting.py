8bd729b923e3eb4d21b25a609f580399253e1349
Sayth <sayth@localhost.localdomain>
2016-05-28 20:27:33 +1000

    fixed command args

--- racemeeting.py
+++ racemeeting.py
@@ -1,26 +1,24 @@
 from pyquery import PyQuery as pq
-#import pandas as pd
+# import pandas as pd
 import psycopg2
 import argparse
 import os
-import glob
 
 
-def GetArgs():
-    '''parse XML from command line'''
-    parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser()
+parser.add_argument("path", type=str, nargs="+")
+parser.add_argument('-e', '--extension', default='',
+                    help='File extension to filter by.')
 
-    parser.add_argument("path", nargs="+")
-    parser.add_argument('-e', '--extension', default='',
-                        help='File extension to filter by.')
-    args = parser.parse_args()
+args = parser.parse_args()
+name_pattern = "*" + args.extension
+my_dir = args.path[0]
 
-    files = set()
-    name_pattern = "*" + args.extension
-    for path in args.path:
-        files.update(glob.glob(os.path.join(path, name_pattern)))
-    return files
-
+for dir_path, subdir_list, file_list in os.walk(my_dir):
+    for name_pattern in file_list:
+        full_path = os.path.join(dir_path, name_pattern)
+        print(full_path)
+        print(file_list)
 
 meetattrs = ('id', 'venue', 'date', 'rail', 'weather', 'trackcondition')
 raceattrs = ('id', 'meeting_id', 'number', 'shortname', 'stage', 'distance',
@@ -33,7 +31,7 @@ conn = psycopg2.connect("")
 with conn, conn.cursor() as cur:
         # First, create tables.
     cur.execute("drop table if exists meetings, races, horses")
-    cur.execute("create table meetings (" +
+    cur.execute("create table meetings (id integer PRIMARY KEY, " +
                 ", ".join("%s varchar" % fld for fld in meetattrs)
                 + ")")
     cur.execute("create table races (" +
@@ -44,18 +42,19 @@ with conn, conn.cursor() as cur:
                 + ")")
 
     # Now walk the tree and insert data.
-    for filename in sorted(GetArgs()):
-        for meeting in pq(filename=filename):
+    for filename in sorted(file_list):
+        for meeting in pq(filename=my_dir + filename):
             meetdata = [meeting.get(attr) for attr in meetattrs]
-            cur.execute("insert into meetings valueme in GetArgs():s (" +
-                        ",".join(["%s"]*len(meetattrs)) + ")", meetdata)
+            cur.execute("insert into meetings values (" +
+                        ",".join(["%s"] * len(meetattrs)) + ")", meetdata)
             for race in meeting.findall("race"):
                 race.set("meeting_id", meeting.get("id"))
                 racedata = [race.get(attr) for attr in raceattrs]
                 cur.execute("insert into races values (" +
-                            ",".join(["%s"]*len(raceattrs)) + ")", racedata)
+                            ",".join(["%s"] * len(raceattrs)) + ")", racedata)
                 for horse in race.findall("nomination"):
                     horse.set("race_id", race.get("id"))
                     horsedata = [horse.get(attr) for attr in horseattrs]
                     cur.execute("insert into horses values (" +
-                                ",".join(["%s"]*len(horseattrs)) + ")", horsedata)
+                                ",".join(["%s"] * len(horseattrs)) + ")",
+                                horsedata)
